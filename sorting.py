6aa228b2de39a3830a79f819d0a25417be09d9cd
anastasieh <272102@vutbr.cz>
2026-04-23 15:20:30 +0200

    treti

--- sorting.py
+++ sorting.py
@@ -10,6 +10,9 @@ def random_numbers(count, low=0, high=100):
     # small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
 
 def selection_sort(cisla_seznam):
+    #     novy_seznam = cisla_seznam.copy()
+    #     novy_seznam.sort()
+    #     return novy_seznam
     seznamicik = cisla_seznam.copy()
     cisilka = len(seznamicik)
     for i in range(cisilka):
@@ -20,9 +23,14 @@ def selection_sort(cisla_seznam):
         seznamicik[i], seznamicik[index_nejmensi] = seznamicik[index_nejmensi], seznamicik[i]
     return seznamicik
 
-#     novy_seznam = cisla_seznam.copy()
-#     novy_seznam.sort()
-#     return novy_seznam
+def bubble_sort(cisla_seznam):
+    seznamek = cisla_seznam.copy()
+    cisilka = len(seznamek)
+    for i in range(cisilka):
+        for j in range(0, cisilka - i - 1):
+            if seznamek[j] > seznamek[j+1]:
+                seznamek[j], seznamek[j+1] = seznamek[j+1], seznamek[j]
+    return seznamek
 #
 if __name__ =="__main__":
     kratky = [5, 1, 4, 2, 8]
@@ -30,6 +38,9 @@ if __name__ =="__main__":
     print(f"{selection_sort(kratky)}")
     puvodni = (selection_sort(random_numbers(20)))
     print(f"{selection_sort(puvodni)}")
+    print(f"{bubble_sort(kratky)}")
+    puvodni = (bubble_sort(random_numbers(20)))
+    print(f"{bubble_sort(puvodni)}")
     # values = random_numbers(20)  #
     # serazena = selection_sort(values)
     # print(serazena)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
